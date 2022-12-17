ffc4cc65c10e922ea507acfb88636856d741cad1
xtreme8000 <xtreme8000@users.noreply.github.com>
2022-12-17 12:30:52 +0100

    Add license header to all files

--- source/daytime.h
+++ source/daytime.h
@@ -1,3 +1,22 @@
+/*
+	Copyright (c) 2022 ByteBit/xtreme8000
+
+	This file is part of CavEX.
+
+	CavEX is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	CavEX is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with CavEX.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #ifndef DAYTIME_H
 #define DAYTIME_H
 
--- source/network/client_interface.c
+++ source/network/client_interface.c
@@ -1,7 +1,24 @@
-#include <gccore.h>
+/*
+	Copyright (c) 2022 ByteBit/xtreme8000
 
-#include "../game/game_state.h"
+	This file is part of CavEX.
+
+	CavEX is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	CavEX is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with CavEX.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include "client_interface.h"
+#include "../game/game_state.h"
 #include "server_interface.h"
 
 #define RPC_INBOX_SIZE 8
